55ef5211ac4e538eec2a4489ec1211dfb4f81187
jochun <jkchun@protonmail.com>
2022-01-16 18:36:52 -1000

    connecting SQL

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+// response from query is captured in 2 variables: (err, rows).  This allows SQL commands to be written in node.js
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+}); ç
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
