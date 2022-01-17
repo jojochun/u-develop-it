469ed3f1f6728908404690ad15af8d4e322e4f03
jochun <jkchun@protonmail.com>
2022-01-16 19:53:00 -1000

    mysql2

--- server.js
+++ server.js
@@ -22,9 +22,37 @@ const db = mysql.createConnection(
 );
 
 // response from query is captured in 2 variables: (err, rows).  This allows SQL commands to be written in node.js
-db.query(`SELECT * FROM candidates`, (err, rows) => {
+/* db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
-}); ç
+}); */
+
+// GET a single candidate
+/* db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+}); */
+
+// Create a candidate
+/*const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+}); */
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
